09f6ab1cdcd4bf95f5effc05697d86f776b0937e
RonitKhokhar <78557509+RonitKhokhar@users.noreply.github.com>
2022-08-23 13:47:37 +0530

    Add files via upload

---
+++ PROJECT1/assets/Js/Home.js
@@ -0,0 +1,36 @@
+
+const icon = document.querySelector('.icon');
+const search = document.querySelector('.search');
+icon.onclick() = function(){
+    search.classList.toggle('active')
+}
+
+
+function Headphnfunc(){
+    location.href=("../views/Headphones.html");
+}
+
+function Mobphnfunc()
+{
+    location.href=("../views/MobilePhones.html");
+}
+
+function Tvfunc()
+{
+    location.href=("../views/Television.html");
+}
+
+function speakerfunc()
+{
+    location.href =("../views/Speakers.html");
+}
+
+function Refrigeratorfunc()
+{
+    location.href=("../views/Refrigerator.html");
+}
+
+function WashMachinefunc()
+{
+    location.href=("../views/WashingMachine.html")
+}
